d78f78a0f0fe1ccbeba7e374c5e88aba05fa15de
Kevin <herrbach.kevin@gmail.com>
2024-08-22 16:24:36 +0900

    Adds icon button to go back to home

--- app/login.tsx
+++ app/login.tsx
@@ -1,3 +1,4 @@
+import { AntDesign } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import { useState } from "react";
@@ -60,30 +61,36 @@ export default function LoginPage() {
       style={{
         flex: 1,
         flexDirection: 'column',
-        justifyContent: "center",
         alignItems: "center",
         padding: 25,
-        gap: 15
       }}
     >
-      <TextInput placeholder="Email" style={styles.input}
-        value={email} onChangeText={text => setEmail(text)} />
-      <TextInput placeholder="Password" secureTextEntry={true}
-        style={styles.input} value={password} onChangeText={text => setPassword(text)} />
-        {!isLogin &&
-          <TextInput placeholder="Confirm password" secureTextEntry={true}
-            style={styles.input} value={confirmPassword} onChangeText={text => setConfirmPassword(text)} />}
-        {!isLogin &&
-          <Pressable style={styles.button} onPress={register}>
-            <Text style={styles.buttonText}>Register</Text>
-          </Pressable>}
-        {isLogin &&
-          <Pressable style={styles.button} onPress={login}>
-            <Text style={styles.buttonText}>Login</Text>
-          </Pressable>}
-        <Pressable onPress={() => setIsLogin(!isLogin)}>
-          <Text style={styles.underlineText}>{isLogin ? 'Register' : 'Login'}</Text>
+      <View style={{flexDirection: 'row', justifyContent: 'flex-end', width: '100%'}}>
+        <Pressable style={{alignSelf: 'baseline'}} onPress={() => router.replace('/')}>
+          <AntDesign name="close" size={24} color="black" />
         </Pressable>
+      </View>
+      <View style={{flex: 1, width: '100%', justifyContent: 'center', gap: 15, alignItems: 'center'}}>
+        <TextInput placeholder="Email" style={styles.input}
+          value={email} onChangeText={text => setEmail(text)} />
+        <TextInput placeholder="Password" secureTextEntry={true}
+          style={styles.input} value={password} onChangeText={text => setPassword(text)} />
+          {!isLogin &&
+            <TextInput placeholder="Confirm password" secureTextEntry={true}
+              style={styles.input} value={confirmPassword} onChangeText={text => setConfirmPassword(text)} />}
+          {!isLogin &&
+            <Pressable style={styles.button} onPress={register}>
+              <Text style={styles.buttonText}>Register</Text>
+            </Pressable>}
+          {isLogin &&
+            <Pressable style={styles.button} onPress={login}>
+              <Text style={styles.buttonText}>Login</Text>
+            </Pressable>}
+          <Pressable onPress={() => setIsLogin(!isLogin)}>
+            <Text style={styles.underlineText}>{isLogin ? 'Register' : 'Login'}</Text>
+          </Pressable>
+      </View>
+
     </View>
   );
 }
